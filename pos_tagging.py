96cc681c5ef2e56639f8a6223887c37579a8d034
DAVIDE RISI <dvdrsi2017@gmail.com>
2023-01-04 20:16:58 +0100

    c

--- pos_tagging.py
+++ pos_tagging.py
@@ -63,19 +63,26 @@ def pos_tagging(R, S, T, E):
     The function returns a dictionary whose keys are the words in S and the values are the roles assigned to these words,
     so that the selected assignment is the one of maximum likelihood.
     """
-    result = {}    
-    probabilities = []
-    indices = []
+    """
+    I used a dynamic programming approach. I consider shorter sentence as sub-problem.
+    """
+    result = {} #
+    probabilities = [] #Matrix constructed by a list of list. It saves the probability of a word to have a certain role. The probability is calculated using the probability of the previous words. 
+    indices = [] #Matrix constructed by a list of list. It saves the index of role of the previous word that has maximized the probability.
     last = len(S)-1
     previous_role = None
 
-    index_to_role = {}
+    index_to_role = {} #In order to convert the indeces to roles, I use this dict. The keys represent an index and the value the corresponding Role from R.
     index = 0
+
+    #Populating the dict for the conversion of the indeces in roles of R.  
     for r in R:
         index_to_role[index] = r
         index = index +1
 
     if(last == 0):
+        #Edge case. If the sentence has one word. 
+        # It is different from the boundary condition because I must consider the probability that the only word of the sentence ends with a specific role. 
         word_index = 0
         probabilities.insert(word_index, list())
         role_index = 0
@@ -83,20 +90,21 @@ def pos_tagging(R, S, T, E):
             weight = E[S[word_index]][r]*T["Start"][r]*T[r]["End"]
             probabilities[word_index].insert(role_index, weight)
             role_index += 1
-    else:
+    else: #All other cases.
         word_index = 0
-        for word in S:
+        for word in S: #Iterating all words in the sentence.
             probabilities.insert(word_index, list()) 
             indices.insert(word_index,list())
             role_index = 0
-            for r in R:
+            for r in R: #For each word, iterating all roles of R.
                 if(word_index == 0):
+                    #Boundary condition. Since I consider shorter sentence as sub-problem, the boundary condition is the sentence with a single word.
                     previous_role = "Start"
                     weight = E[word][r]*T[previous_role][r]
                     probabilities[word_index].insert(role_index, weight)
 
                 elif(word_index == last):
-                    
+                    #If the word is the last I must consider the probability that the word ends with the role r. 
                     max,max_index = _get_max_final_word(probabilities[word_index-1],R,r,T)
                     weight = max*E[word][r]
                     probabilities[word_index].insert(role_index, weight)
@@ -113,13 +121,13 @@ def pos_tagging(R, S, T, E):
     #print(probabilities)
 
     #Finding the max probability of the last word in the probabilities matrix.
-    #Starting from the last word I can reconstruct the solution.
+    #I choose the role of the last word with the highest probability and proceed backward reconstructing the solution.
     maxValue,max_index = _get_max_from_list(probabilities[len(S)-1])
 
     length = len(S)-2
     result[S[-1]] = index_to_role[max_index]
-
-    for i in range(length,-1,-1):
+    # Finding the solution using the indeces matrix.
+    for i in range(length,-1,-1): #It stops at 0.
         temp = indices[i+1][max_index]
         result[S[i]] = index_to_role[indices[i+1][max_index]]
         max_index = temp
